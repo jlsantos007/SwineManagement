c02b0ded8098766b34882cc659bd21a3b9992021
ngkirito <jlsantos007.lebie@gmail.com>
2017-07-14 19:14:37 +0800

    changes

--- app/src/main/java/com/csthesis/swinemanagement/Herd.java
+++ app/src/main/java/com/csthesis/swinemanagement/Herd.java
@@ -9,7 +9,6 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
